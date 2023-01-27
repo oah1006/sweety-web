af3774a34fe2b8f2b46b0896916c74cabe2c79bb
OaH <81689054+oah1006@users.noreply.github.com>
2023-01-27 22:19:01 +0700

    first commit

---
+++ src/views/admin/auth/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="flex flex-col items-center justify-center w-full min-h-screen">
+        
+        <div class="border border-solid shadow-md h-96 w-96 ">
+            <p class="text-center mt-4 font-medium text-lg">Login</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
