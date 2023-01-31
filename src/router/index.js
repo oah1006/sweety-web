fd9b2c96e52c9ab0e5a6b140dbeb4a9f6668cc97
OaH <81689054+oah1006@users.noreply.github.com>
2023-01-31 20:18:05 +0700

    create route login, dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+// import auth/ login view
 import Login from '../views/admin/auth/Login.vue'
+
+import Dashboard from '../views/admin/Dashboard.vue'
+
+import ToastNotification from '../components/ToastNotification.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'login',
       component: Login
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
     },
   ]
 })
