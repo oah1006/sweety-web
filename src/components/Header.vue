a89d35224cc4722285b79877640c727f81932907
OaH <81689054+oah1006@users.noreply.github.com>
2023-02-08 12:46:31 +0700

    create header component

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex items-center shadow">
+        <div class="w-1/6 border-r-2 border-solid text-center flex-none">
+            <img src="../images/logo.png" alt="" class="w-14 h-14 object-cover mx-auto" />
+        </div>
+        <div class="flex ml-auto grow">
+            <div class="flex gap-3 items-center ml-auto pr-10 h-full">
+                <div class="rounded-full h-8 w-8 border-solid border-1 border"></div>
+                <p class="text-orange-500 font-medium text-base">Nhật Hào</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
