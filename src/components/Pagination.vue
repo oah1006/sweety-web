6998201e8578a708534ffb0c4937c505a3436bbf
oah1006 <bnhao10062001@gmail.com>
2023-02-27 01:44:40 +0700

    props and emits

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,21 +1,36 @@
 <template>
     <div class="h-12 flex items-center justify-between px-4">
-        <p class="text-zinc-400">Trước</p>
+        <a class="text-zinc-400 cursor-pointer" @click="prePage">Trước</a>
         <div class="flex gap-2 items-center">
             <input type="text" class="form-input w-7 px-2 py-0.5 text-center" />
             <p>/ 4</p>
         </div>
-        <p class="text-zinc-400">Sau</p>
+        <a class="text-zinc-400 cursor-pointer" @click="nextPage">Sau</a>
     </div>
 </template>
 
 <script setup>
 
 const props = defineProps({
-    total: Number
+    total: Number,
+    per_page: Number,
+    page: Number
 })
 
-console.log(props.total)
+const emits = defineEmits(['pre-page'])
+
+function prePage() {
+    emits('pre-page')
+}
+
+function nextPage() {
+    emits('next-page')
+}
+
+
+
+
+
 
 </script>
 
