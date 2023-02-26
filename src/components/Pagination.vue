f5b112e30adbc506dc2ce3b570e432ea3198caa5
OaH <81689054+oah1006@users.noreply.github.com>
2023-02-26 18:08:29 +0700

    chore

---
+++ src/components/Pagination.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="h-12 flex items-center justify-between px-4">
+        <p class="text-zinc-400">Trước</p>
+        <div class="flex gap-2 items-center">
+            <input type="text" class="form-input w-7 px-2 py-0.5 text-center" />
+            <p>/ 4</p>
+        </div>
+        <p class="text-zinc-400">Sau</p>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    total: Number
+})
+
+console.log(props.total)
+
+</script>
+
