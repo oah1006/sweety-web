e65088ba3dba0ba0f7ebf6829b1b3ad472268f48
OaH <81689054+oah1006@users.noreply.github.com>
2023-03-12 18:20:30 +0700

    update button component

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="clickRedirect" class="flex gap-3 items-center px-4 py-2 text-white text-center bg-orange-500 rounded-xl mr-12">
+  <button @click="clickRedirect" class="flex gap-3 items-center px-4 py-2 text-white text-center bg-orange-500 rounded-xl">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-white w-5 h-5">
       <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
     </svg>
