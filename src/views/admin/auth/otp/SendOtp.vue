b415f080bbd5a4584646e12b045e8b45aea74b21
oah1006 <bnhao10062001@gmail.com>
2023-03-22 02:43:21 +0700

    create view send otp and verify otp

---
+++ src/views/admin/auth/otp/SendOtp.vue
@@ -0,0 +1,25 @@
+<template>
+  <ForgotLayout>
+    <template #form-forgot-password>
+      <FormForgotPasswordLayout textButton="Gửi OTP" title="Bạn quên mật khẩu?" subTitle="Đừng lo lắng, hãy nhập email phía bên dưới để được nhận mã OTP xác thực người dùng và đổi mật khẩu nhé!">
+        <template #box-input>
+          <InputBox name="Email" margin="mt-4 mb-1">
+            <template #input>
+              <InputEmail v-model:modelEmail="email" />
+            </template>
+          </InputBox>
+        </template>
+      </FormForgotPasswordLayout>
+    </template>
+  </ForgotLayout>
+</template>
+<script setup>
+import ForgotLayout from "@/components/layouts/ForgotLayout.vue";
+import FormForgotPasswordLayout from "@/components/layouts/FormForgotPasswordLayout.vue";
+import InputEmail from "@/components/inputs/InputEmail.vue";
+import InputBox from "@/components/layouts/BoxInputLayout.vue";
+
+import { ref } from "vue";
+
+const email = ref('')
+</script>
---
+++ src/views/admin/auth/otp/VerifyOtp.vue
@@ -0,0 +1,35 @@
+<template>
+  <ForgotLayout>
+    <template #form-forgot-password>
+      <FormForgotPasswordLayout textButton="Xác nhận OPT" title="Xác thực mã OTP" subTitle="Vui lòng nhập mã OTP 6 chữ số đã được gửi đến email của bạn. Mã OTP sẽ có hiệu lực trong 1 phút">
+        <template #box-input>
+          <InputBox margin="mt-4">
+            <template #input>
+              <InputOtp v-model:modelOtp="otp" />
+            </template>
+          </InputBox>
+          <InputBox>
+            <template #input>
+              <InputEmail type="hidden" v-model:modelEmail="email" />
+            </template>
+          </InputBox>
+        </template>
+      </FormForgotPasswordLayout>
+    </template>
+  </ForgotLayout>
+</template>
+
+<script setup>
+
+import ForgotLayout from "@/components/layouts/ForgotLayout.vue";
+import FormForgotPasswordLayout from "@/components/layouts/FormForgotPasswordLayout.vue";
+import InputEmail from "@/components/inputs/InputEmail.vue";
+import InputBox from "@/components/layouts/BoxInputLayout.vue";
+import InputOtp from "@/components/inputs/InputOtp.vue";
+
+import { ref } from "vue";
+
+const otp = ref('')
+const email = ref('')
+
+</script>
