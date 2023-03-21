e3eeb61e9cdbba7be58942a86340be54829bac3b
oah1006 <bnhao10062001@gmail.com>
2023-03-22 02:42:47 +0700

    create forgot passowrd component

---
+++ src/components/layouts/ForgotLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full h-screen min-h-full bg-slate-200">
+    <div class="flex items-center justify-center h-screen">
+      <slot name="form-forgot-password"></slot>
+    </div>
+  </div>
+</template>
---
+++ src/components/layouts/FormForgotPasswordLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <form @submit.prevent="handleSubmit" class="w-2/5 px-20 py-8 bg-white rounded-lg">
+    <p class="text-center font-medium text-3xl">{{ props.title }}</p>
+    <p class="text-center text-base font-thin mt-1">{{ props.subTitle }}</p>
+    <slot name="box-input"></slot>
+    <button class="px-4 py-2 bg-orange-500 w-full mt-4 rounded-sm text-white">{{ props.textButton }}</button>
+  </form>
+</template>
+
+<script setup>
+  const props = defineProps({
+    title: String,
+    subTitle: String,
+    textButton: String
+  })
+</script>
