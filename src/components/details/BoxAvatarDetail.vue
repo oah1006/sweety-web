425a5fe4c98ee3626f4b7e17cc7b8664d1ca1f0f
OaH <81689054+oah1006@users.noreply.github.com>
2023-06-28 12:15:46 +0700

    up github

--- src/components/details/BoxAvatarDetail.vue
+++ src/components/details/BoxAvatarDetail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-8 flex flex-col items-center justify-center px-10 py-4">
-    <img v-if="props.url" :src="props.url" class="w-32 h-32 object-cover rounded-full text-center"/>
+    <img v-if="props.url" :src="urlApi + props.url" class="w-32 h-32 object-cover rounded-full text-center"/>
     <img v-else src="../../images/no-avatar.jpg" class="w-32 h-32 object-cover rounded-full text-center"/>
     <p class="text-zinc-500 font-bold mt-3 text-lg">{{ props.item }}</p>
     <BoxItemRole class="mt-2" :role="props.role"/>
@@ -10,6 +10,8 @@
 <script setup>
 
 import BoxItemRole from "@/components/details/BoxItemRole.vue";
+
+const urlApi = process.env.VUE_APP_ENV_URL
 
 const props = defineProps({
   nameLabel: String,
